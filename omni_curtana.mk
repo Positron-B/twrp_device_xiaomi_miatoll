7ef21ad4e7d221182587388642eb8ea33c7ac0a4
Alexander Winkowski <dereference23@outlook.com>
2021-10-12 13:55:44 +0000

    curtana: Clean up deprecated bits
    Change-Id: If1253c09f958d4e25a6759f84b0fec6f160393d3

--- omni_curtana.mk
+++ omni_curtana.mk
@@ -18,9 +18,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from curtana device
 $(call inherit-product, device/xiaomi/curtana/device.mk)
