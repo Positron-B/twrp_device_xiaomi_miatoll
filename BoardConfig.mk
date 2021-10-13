7d0308d1eeb283a4da1ee78fac01f40b2ec49e6f
Alexander Winkowski <dereference23@outlook.com>
2021-10-13 12:56:10 +0000

    curtana: Clean up TWRP flags
     * TW_INPUT_BLACKLIST is not needed on this device
     * TW_SCREEN_BLANK_ON_BOOT is not needed on this device
     * TW_USE_TOOLBOX is set to true by default
    
    Change-Id: I36b343d9c0dc1dab8b6813edea8a5de4da4d7889

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -97,6 +97,3 @@ PLATFORM_VERSION := 16.1.0
 # TWRP Configuration
 TW_THEME := portrait_hdpi
 TW_EXTRA_LANGUAGES := true
-TW_SCREEN_BLANK_ON_BOOT := true
-TW_INPUT_BLACKLIST := "hbtp_vm"
-TW_USE_TOOLBOX := true
