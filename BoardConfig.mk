f79f5be8e77edf20ec016c2750700c8552398f7a
Alexander Winkowski <dereference23@outlook.com>
2021-11-03 11:46:50 +0000

    miatoll: Disable audit
    It spams logs too much.
    
    Change-Id: Ia910ce3c0d74d6902be1085ce8044ec35a4f1ad7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,7 +33,7 @@ TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_miatoll
 BOARD_BOOTIMG_HEADER_VERSION := 2
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom swiotlb=1 androidboot.usbcontroller=a600000.dwc3
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom swiotlb=1 androidboot.usbcontroller=a600000.dwc3 audit=0
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 BOARD_PREBUILT_DTBIMAGE_DIR := $(DEVICE_PATH)/prebuilt
