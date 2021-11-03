9482b8a2405095cb6072e10ebb0ad77d3cc1c3fe
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-11-03 12:16:55 +0000

    miatoll: Add libinit for variants handling
    [dereference23: Adapt for TWRP]
    
    Change-Id: Icec1c447c1fd704c265513250092deac34195132

---
+++ libinit/init_xiaomi_miatoll.cpp
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2021-2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <libinit_variant.h>
+#include "vendor_init.h"
+
+static const variant_info_t curtana_info = {
+    .brand = "Redmi",
+    .device = "curtana",
+    .model = "Redmi Note 9S",
+};
+
+static const variant_info_t excalibur_info = {
+    .brand = "Redmi",
+    .device = "excalibur",
+    .model = "Redmi Note 9 Pro Max",
+};
+
+static const variant_info_t gram_info = {
+    .brand = "POCO",
+    .device = "gram",
+    .model = "POCO M2 Pro",
+};
+
+static const variant_info_t joyeuse_info = {
+    .brand = "Redmi",
+    .device = "joyeuse",
+    .model = "Redmi Note 9 Pro",
+};
+
+static const std::vector<variant_info_t> variants = {
+    curtana_info,
+    excalibur_info,
+    gram_info,
+    joyeuse_info,
+};
+
+void vendor_load_properties() {
+    search_variant(variants);
+}
