c4d0d16b3c1778a6e02fa91830e44b29cf8d98bd
Alexander Winkowski <dereference23@outlook.com>
2021-11-04 05:35:19 +0000

    miatoll: Bring up decryption
    * Blobs were extracted from lineage-18.1-20220617-nightly-miatoll-signed.zip
    
    Change-Id: Id6359dfb7f09ebc4c190b0f8c71a33689694def5

--- device.mk
+++ device.mk
@@ -13,6 +13,17 @@ PRODUCT_SHIPPING_API_LEVEL := 29
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# Decryption
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.crypto.dm_default_key.options_format.version=2 \
+    ro.crypto.volume.filenames_mode=aes-256-cts \
+    ro.crypto.volume.metadata.method=dm-default-key \
+    ro.crypto.volume.options=::v2
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
