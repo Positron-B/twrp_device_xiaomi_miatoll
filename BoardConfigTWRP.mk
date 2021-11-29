83743961597d8f554cd5498ababf9312e839bda3
Albert I <kras@raphielgang.org>
2021-11-30 00:50:50 +0800

    miatoll: Define brightness-related flags for TWRP
    Saving your time for recursive search of sysfs :)
    
    Signed-off-by: Albert I <kras@raphielgang.org>
    Change-Id: I6eab8dca82f36cfea6af6f40073c6f4981b78cbf

--- BoardConfigTWRP.mk
+++ BoardConfigTWRP.mk
@@ -7,6 +7,11 @@
 # APEX
 TW_EXCLUDE_APEX := true
 
+# Brightness
+TW_BRIGHTNESS_PATH := /sys/class/backlight/panel0-backlight/brightness
+TW_DEFAULT_BRIGHTNESS := 2744
+TW_MAX_BRIGHTNESS := 4095
+
 # Hack: prevent anti rollback
 PLATFORM_SECURITY_PATCH := 2099-12-31
 PLATFORM_VERSION := 16.1.0
