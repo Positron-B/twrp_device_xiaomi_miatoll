14216855c2c7e67c72149b1b0c18612c7695f299
Alexander Winkowski <dereference23@outlook.com>
2022-09-27 16:06:36 +0000

    curtana: Remove unused flags
    Change-Id: I3a26aa938af94934f92c5f7352a1ba376cb1365c

--- omni_curtana.mk
+++ omni_curtana.mk
@@ -20,10 +20,3 @@ PRODUCT_NAME := omni_curtana
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 9S
 PRODUCT_MANUFACTURER := xiaomi
-
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="curtana_global-user 12 RKQ1.211019.001 V13.0.2.0.SJWMIXM release-keys"
-
-BUILD_FINGERPRINT := Redmi/curtana_global/curtana:12/RKQ1.211019.001/V13.0.2.0.SJWMIXM:user/release-keys
